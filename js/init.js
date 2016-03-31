2e6648a184b3f451e28b7163d7be77cfbf87469a
IceYoshi <mike_pereira100000@yahoo.de>
2016-03-31 19:47:11 +0200

    Enable Error Display

--- js/init.js
+++ js/init.js
@@ -3,11 +3,6 @@ ServerConnection.init("example.lu/rest/");
 
 window.addEventListener('resize', resize, false);
 
-window.onerror = function(error) {
-  // show error message when there is no console access (e.g. on mobile devices)
-  alert(error);
-};
-
 function init() {
   StageManager.idle();
 }
