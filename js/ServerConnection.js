f20f671bc40304481d3738007682db59abe77b00
IceYoshi <mike_pereira100000@yahoo.de>
2016-05-04 22:04:25 +0200

    Added socket connection remote control

--- js/ServerConnection.js
+++ js/ServerConnection.js
@@ -1,21 +1,32 @@
 class ServerConnection {
   static init(serverAddress) {
-    this._server = serverAddress;
+    if(serverAddress) {
+      let socket = new WebSocket(serverAddress);
+
+      socket.onopen = function() { console.log("Socket connection opened"); };
+      socket.onmessage = function(event) {
+        this.receive(JSON.parse(event.data));
+      }.bind(this);
+      socket.onclose = function() { console.log("Socket connection closed"); };
+
+      this._socket = socket;
+    } else {
+      console.log("No socket destination address. Proceeding at localhost");
+    }
+
     this._isInitialized = true;
   }
 
   static send(data) {
     if(!this._isInitialized) return false;
-
-    // TODO: send data to server
     console.log('To server: ' + JSON.stringify(data));
+    if(this._socket) this._socket.send(JSON.stringify(data));
 
     return true;
   }
 
   static receive(data) {
     if(!this._isInitialized) return false;
-
     console.log('From server: ' + JSON.stringify(data));
     MessageInterpreter.interpret(data);
 
